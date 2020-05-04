47e1f088e8e4567f9fdee1138241f170a7499eaa
julianwakefield <julianwakefield@outlook.com>
2020-05-04 11:06:18 +0100

    change migration

--- db/migrate/20200503113544_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserTable < ActiveRecord::Migration[5.0]
-  def change
-        create_table :users do |t|
-          t.string :name
-          t.integer :wallet
-        end
-      end
-   
-end
