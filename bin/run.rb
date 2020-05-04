7ea21f76bdd6beef1e1308c787f5b590b9e10dd6
julianwakefield <julianwakefield@outlook.com>
2020-05-04 17:11:01 +0100

    anderson  changes

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,24 @@
 require_relative '../config/environment'
 cli = Application.new
 
- cli.welcome
- cli.main_menu
+cli.welcome
+cli.main_menu
+    
+# def search_by_property(prop,value)
+#     if prop == "byNight"
+#         if Property.min(:price).value < value
+            
+#             Property.all.where("#{prop} >= ?", value).limit(5)
+#         else
+            
+#             Property.all.where("#{prop} <= ?", value).limit(5)
+#         end
+        
+#     else
+        
+#     end
+# end
+
 
 
 puts "HELLO WORLD"
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,11 +2,13 @@ require "pry"
 class Application
 
     attr_reader :prompt
+    attr_accessor :user
 
     
     def initialize()
         @prompt = TTY::Prompt.new
         @tty_prompt= TTY::Prompt.new
+        @user = nil
     end
 
     def user_input
@@ -25,16 +27,29 @@ class Application
     end
 
     def main_menu
+        quit = false
         system "clear"
-        puts "Hello, #{}!"
+        puts "Hello, #{@user.name}!"
          puts ""
+         while !quit
+             
+         
          choice = self.prompt.select("Welcome! Please select one of the following:") do |menu|
              # create a booking?
-             menu.choice "My Bookings", -> {self.user.Booking}
-             menu.choice "Manage Bookings", -> {self.user.manage_account}
-             menu.choice "Search Bookings", -> {self.search}
+             menu.choice "Make a Booking", -> {booking_menu} 
+                    # allows to search properties and make a new booking
+                    # search properites - list of all properties
+                        
+                        # search by wifi, bedrooms, self-catering, price - list all -- menu
+                        # don't want to list any that are FULL
+                        # create the booking if fully paid
+             menu.choice "Manage My Bookings", -> {self.user.Booking}
+                #  menu.choice "Manage Bookings", -> {self.user.manage_account}
+                    # update booking
+                    #  cancel booking
              menu.choice "Exit", -> {Application.exit_app}
          end
+        end
     end
 
     def self.exit_app
@@ -49,9 +64,11 @@ class Application
 
     def new_user
         reply = self.prompt.ask("What is your name? (Please enter your name and hit enter twice to confirm)")
-        reply = user_input
-        User.find_or_create_by(name: reply)
+        @user = User.new(name: reply)
+        #User.find_or_create_by(name: reply)
+        
         main_menu
+        # needs money in wallet
         
     end
  
@@ -59,13 +76,37 @@ class Application
     def returning_user
         reply = self.prompt.ask("What is your name? (Please enter your name and hit enter twice to confirm)")
         reply = user_input
-        #User.all.find{|user| user.name == reply}
+        User.all.find{|user| user.name == reply}
     end
 
         
+    def booking_menu
+        system "clear"
+        choice = self.prompt.select("Search by:") do |menu|
+           
+            # create a booking?
+            menu.choice "No. of Bedrooms", -> {self.search_properties}
+            menu.choice "Self-catering?", -> {self.search_properties}
+            menu.choice "Price per night", -> {self.search_properties} 
+            menu.choice "Wifi", -> {self.search_properties}
+            menu.choice "All Properties", -> {search_all_properties}
+            menu.choice "Main Menu", -> {}
+        end
+    end
 
+    # def properties
+    #     Property.all.map{|property| property.}
 
+    # end
 
+    # def search_all_properties
+    #     Property.all
+    # end
+
+    # def no_of_bedrooms
+    #     puts "how many bedrooms?"
+    #     reply = user_input 
+    # end
         
         
         
